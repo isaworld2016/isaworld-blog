5693f8d268e9f2b62bbc8c81b1cf46843a56372f
isaworld <isaworld@efit.co.kr>
2022-11-01 16:01:14 +0900

    feat: 페이지네이션의 구현3

--- data/metadata.ts
+++ data/metadata.ts
@@ -4,7 +4,7 @@ const metadata: IMetadata = {
   title: "Isaworld Blog",
   description: "천재적인 이자님의 블로그",
   author: "isaworld",
-  rowsPerPage: 3
+  rowsPerPage: 1
 };
 
 export default metadata;
--- pages/blog.tsx
+++ pages/blog.tsx
@@ -1,25 +1,65 @@
-import { allPosts } from "contentlayer/generated";
+import { allPosts, Post } from "contentlayer/generated";
 import metadata from "data/metadata";
 import { InferGetStaticPropsType } from "next";
+import React, { useEffect, useState } from 'react';
 import BlogPost from "../components/BlogPost";
 import Container from "../components/Container";
 
 const Blog = (props: InferGetStaticPropsType<typeof getStaticProps>) => {
-  const showPage = Math.ceil(
-    props.pagination?.totalCount / metadata.rowsPerPage
-  );
-  
-  const tempArr = Array.from({ length: showPage }, (v, i) => i + 1);
+  const [currPage, setCurrPage] = useState(1);
+  const [isShowMaxPage, setShowMaxPage] = useState(false);
 
-  const callPage = (event: React.MouseEvent<HTMLElement>, page: number): void => {
-    console.log("callPage", page);
+  const calFromPage = (param: number): number => {
+    return metadata.rowsPerPage * (param - 1);
+  }
+  const [fromPage, setFromPage] = useState(calFromPage(currPage));
+
+  const calToPage = (param: number): number => {
+    return metadata.rowsPerPage * param;
+  }
+  const [toPage, setToPage] = useState(calToPage(currPage));
+
+  const maxPage = Math.ceil(
+      props.pagination?.totalCount / metadata.rowsPerPage
+    );
+
+  useEffect(() => {
+    setToPage(calToPage(currPage));
+    setFromPage(calFromPage(currPage));
+  }, [currPage])
+
+  const postSlider = (posts: Post[]): Post[] => {
+    return props.posts.slice(fromPage, toPage)
+  }
+
+  // TODO: 뭔가 이상하다...
+  const showPages = () => {
+    let centerArray = Array.from({ length: maxPage }, (v, i) => String(i + 1));
+    // 차가 2 이상이면
+    // if(maxPage - (currPage + 3) >= 2) {
+    //   centerArray = Array.from({ length: currPage + 3 }, (v, i) => String(i + 1)); // 문자열 취급
+    //   centerArray.push('...'); // 말줄임 추가
+    //   centerArray.push(String(maxPage)) // 마지막 페이지 추가
+    // }
+    // 현재 페이지에서 3페이지 앞이...
+    // if (currPage - 3 > 2) {
+    //   // 앞에 세자리만 남기고 잘라야 하는디
+    //     centerArray.splice(1, 0, '... '); // 말줄임 추가
+    // }
+    return centerArray;
+  }
+
+  const callPage = (e: React.MouseEvent<HTMLElement>, page: number): void => {
+    if(page >= maxPage + 1) return;
+    setCurrPage(page);
   };
 
   return (
     <Container>
       <div className={`mt-10 flex flex-col`}>
         <div>total: {props.pagination?.totalCount}</div>
-        {props.posts.map((post) => (
+        <div>perPage: {metadata.rowsPerPage}</div>
+        {postSlider(props.posts).map((post) => (
           <BlogPost
             date={post.date}
             title={post.title}
@@ -28,11 +68,23 @@ const Blog = (props: InferGetStaticPropsType<typeof getStaticProps>) => {
             key={post._id}
           />
         ))}
-        {tempArr.map((a) => (
-          <a key={a} onClick={(event) =>callPage(event, a)}>
-            {a}
-          </a>
-        ))}
+        <a>
+          <span onClick={e => callPage(e, 1)}>{'<<'}</span>
+          <span onClick={e => callPage(e, currPage - 1)}> {'<'} </span>
+          {showPages().map((a) => (
+            <span key={a} onClick={(e) => callPage(e, Number(a))} className={"mr-5" + (Number(a) == currPage ? ' text-rose-500': '')}>
+              {a}
+            </span>
+          ))}
+          {isShowMaxPage ? '...' : null}
+          {isShowMaxPage ?
+            <span onClick={(e) => callPage(e, maxPage)}> {maxPage}</span>
+            : null
+          }
+
+          <span onClick={e => callPage(e, currPage + 1)}>{'>'} </span>
+          <span onClick={e => callPage(e, maxPage)}>{'>>'}</span>
+        </a>
       </div>
     </Container>
   );
@@ -42,14 +94,10 @@ export const getStaticProps = async () => {
   let posts = allPosts.sort(
     (a, b) => Number(new Date(b.date)) - Number(new Date(a.date))
   );
-  const currPage = 1;
-
-  const fromPage = metadata.rowsPerPage * (currPage - 1);
-  const toPage = metadata.rowsPerPage * currPage + 1;
 
   return {
     props: {
-      posts: posts.slice(fromPage, toPage),
+      posts,
       pagination: {
         totalCount: allPosts.length,
       },
