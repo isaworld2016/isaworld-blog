dbfefa15c5de591112b4c38033e999723905deef
isaworld <isaworld@efit.co.kr>
2022-11-03 10:32:46 +0900

    chore: Icon 회전 테스트 2

--- public/assets/icons/First.tsx
+++ public/assets/icons/First.tsx
@@ -3,7 +3,7 @@ import { IEndIcon } from "./interface/IIcon";
 function FirstIcon({ width, height, fill, toLeftYn }: IEndIcon) {
   return (
     <svg
-    transform="rotate(180)"
+    transform="rotate(45)"
     xmlns="http://www.w3.org/2000/svg"
     width={width}
       height={height}
