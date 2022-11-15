a14a8cca4cedcf9395a5cd4e7f3110ae33f49bde
isaworld <isaworld@efit.co.kr>
2022-11-15 16:13:34 +0900

    feat: 중간커밋5

--- components/PostList.tsx
+++ components/PostList.tsx
@@ -4,12 +4,11 @@ import { Post } from "contentlayer/generated";
 const PostList = ({ posts, totalCount }: { posts: Post[]; totalCount: number }) => {
   return (
     <>
-      <div className={`border-b-2 pt-20 pb-1`}>
-        <span>
-          검색결과{" "}
-          <span className="text-red-400 font-bold">{totalCount}</span>
+      <div className={`border-b-2 w-2/3 pt-20 pb-1`}>
+        <section>
+          검색결과 <span className="text-red-400 font-bold">{totalCount}</span>
           건
-        </span>
+        </section>
       </div>
       {posts.map((post: Post) => (
         <BlogPost
--- pages/blog.tsx
+++ pages/blog.tsx
@@ -1,7 +1,5 @@
 import { allPosts, Post } from "contentlayer/generated";
-import metadata from "data/metadata";
 import { InferGetStaticPropsType } from "next";
-import React, { useState } from 'react';
 import PostList from "../components/PostList";
 import SearchBar from "../components/SearchBar";
 import Container from "../components/Container";
@@ -10,31 +8,13 @@ import usePagination from "hooks/usePagination";
 
 const Blog = ({ posts }: InferGetStaticPropsType<typeof getStaticProps>) => {
    const { fromPage, toPage } = usePagination();
-  // const [currPage, setCurrPage] = useState<number>(1);
-  // const calPage = {
-  //   from(param: number): number {
-  //     return metadata.rowsPerPage * (param - 1);
-  //   },
-  //   to(param: number): number {
-  //     return metadata.rowsPerPage * param;
-  //   },
-  // };
-  // const [fromPage, setFromPage] = useState<number>(calPage.from(currPage));
-  // const [toPage, setToPage] = useState<number>(calPage.to(currPage));
 
   return (
     <Container>
       <div className={`mt-10 flex flex-col`}>
         <SearchBar />
         <PostList posts={posts.slice(fromPage, toPage)} totalCount={posts.length} />
-        <Pagination
-          totalCount={posts.length}
-          // setCurrPage={setCurrPage}
-          // currPage={currPage}
-          // setFromPage={setFromPage}
-          // setToPage={setToPage}
-          // calPage={calPage}
-        />
+        <Pagination totalCount={posts.length} />
       </div>
     </Container>
   );
