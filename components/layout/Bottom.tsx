d39af94213ba93faeaf503c808aa4c240d2e9b87
isaworld <isaworld@efit.co.kr>
2022-12-12 17:30:48 +0900

    chore: 불필요한 import 제거.

--- components/layout/Bottom.tsx
+++ components/layout/Bottom.tsx
@@ -1,10 +1,6 @@
-import Image from "next/image";
-
 const Bottom = () => {
   return (
-    <div
-      className="mt-5 mb-1"
-    >
+    <div className="mt-5 mb-1">
       ©2022 isaworld - All Right Reserved
     </div>
   );
--- pages/about.tsx
+++ pages/about.tsx
@@ -40,5 +40,4 @@ const About = () => {
   );
 };
 
-
 export default About;
